ce7f51a2ffce7c4e139dc96276ac769bf6f882d2
mertcanft <mertcanft@gmail.com>
2022-05-22 22:03:26 +0300

    2 comment

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -26,7 +26,7 @@ const Blog = () => {
         <div className="blog-wrap">
           <header>
             <p className="blog-date">Published {blog.createdAt}</p>
-            <h2>{blog.title}</h2>
+            <h1>{blog.title}</h1>
             <div className="blog-subCategory">
               {blog.subCategory.map((category, i) => (
                 <div key={i}>
